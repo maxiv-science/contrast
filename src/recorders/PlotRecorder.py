cd829b1d7cd95f7267008865540f9277e842880e
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2018-09-18 09:20:33 +0200

    Improved the plotter recorder

--- src/recorders/PlotRecorder.py
+++ src/recorders/PlotRecorder.py
@@ -7,22 +7,29 @@ plt.ion()
 
 class PlotRecorder(Recorder):
 
-    def __init__(self, xdata, ydata, name='plot'):
+    def __init__(self, data1, data2=None, name='plot'):
         #super(PlotRecorder, self).__init__(name='plotrecorder')
         Recorder.__init__(self, name=name)
-        self.xdata = xdata
-        self.ydata = ydata
+        if data2 is not None:
+            self.xdata = data1
+            self.ydata = data2
+        else:
+            self.xdata = None
+            self.ydata = data1
         self.nplots = 0
 
     def init(self):
         self.fig = plt.figure()
         self.fig.canvas.set_window_title(self.name)
         self.ax = self.fig.gca()
+        self.ax.set_xlabel(self.xdata)
+        self.ax.set_ylabel(self.ydata)
         plt.pause(.1)
         self.scannr = None
 
     def act_on_data(self, dct):
         if not dct['scannr'] == self.scannr:
+            # start a new scan
             self.nplots += 1
             self.scannr = dct['scannr']
             self.x, self.y = [], []
@@ -30,11 +37,16 @@ class PlotRecorder(Recorder):
             self.l = Line2D(xdata=[], ydata=[], color=col)
             self.ax.add_line(self.l)
         try:
-            self.x.append(dct[self.xdata])
+            # if our data isn't in dct, just move on
             self.y.append(dct[self.ydata])
+            if self.xdata is not None:
+                # checking if we have explicit x data
+                self.x.append(dct[self.xdata])
+            else:
+                self.x = range(len(self.y))
+            self.l.set_data(self.x, self.y)
         except KeyError:
             return
-        self.l.set_data(self.x, self.y)
         self.ax.relim()
         self.ax.autoscale_view()
         plt.draw()
@@ -49,16 +61,16 @@ class PlotRecorder(Recorder):
 
 @macro
 class LivePlot(object):
-    def __init__(self, xdata, ydata):
+    def __init__(self, data1, data2=None):
         basename = 'name'
         name = basename
         i = 2
         while name in [r.name for r in Recorder.getinstances()]:
             name = basename + '_%d' % i
             i += 1
-        self.xdata = xdata.name
-        self.ydata = ydata.name
+        self.data1 = data1.name if hasattr(data1, 'name') else data1
+        self.data2 = data2.name if hasattr(data2, 'name') else data2
         self.name = name
     def run(self):
-        rec = PlotRecorder(self.xdata, self.ydata, self.name)
+        rec = PlotRecorder(data1=self.data1, data2=self.data2, name=self.name)
         rec.start()
