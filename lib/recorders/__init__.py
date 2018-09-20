b6f5e4f08e84f5cae2f0e415fd08dbcb5fe3c1ae
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2018-09-20 09:27:01 +0200

    Even more careful importing

--- lib/recorders/__init__.py
+++ lib/recorders/__init__.py
@@ -4,4 +4,8 @@ try:
 except ImportError:
     print('Note: PlotRecorder not available, probably because matplotlib is missing.')
 
-from .Hdf5Recorder import Hdf5Recorder
+try:
+    from .Hdf5Recorder import Hdf5Recorder
+except ImportError:
+    print("Note: Hdf5Recorder not available, probably because h5py is missing.")
+
