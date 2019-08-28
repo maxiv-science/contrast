1db28714584d5c99d981ad65650c113a9ca676b2
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-08-28 21:29:47 +0200

    Cleaned up the spiral scan a bit

--- lib/scans/Spiral.py
+++ lib/scans/Spiral.py
@@ -1,11 +1,12 @@
 from .Scan import SoftwareScan
+from .AScan import DScan
 from ..environment import macro, MacroSyntaxError
 from ..motors import all_are_motors
 import numpy as np
 import time
 
 @macro
-class SpiralScan(SoftwareScan):
+class SpiralScan(DScan):
     """
     Software scan across a 2D Archimedes spiral centered on the 
     current position.
@@ -13,39 +14,25 @@ class SpiralScan(SoftwareScan):
     spiralscan <motor1> <motor2> <stepsize> <positions> <exp_time>
     """
 
-    def __init__(self, *args):
+    def __init__(self, m1, m2, stepsize, npos, exptime):
         """
-        Parse arguments.
+        Parse arguments. We're inheriting DScan to get its nice run()
+        method, but we'll call the SoftwareScan constructor anyway bacause
+        we're not interested in DScan's way of parsing arguments.
         """
         try:
-            assert len(args) == 5
-            super(SpiralScan, self).__init__(float(args[4]))
-            self.motors = args[:2]
-            self.stepsize = float(args[2])
-            self.npos = int(args[3])
+            SoftwareScan.__init__(self, float(exptime))
+            self.motors = [m1, m2]
+            self.stepsize = float(stepsize)
+            self.npos = int(npos)
             assert all_are_motors(self.motors)
         except:
             raise MacroSyntaxError
 
     def _generate_positions(self):
+        starting = [m.position() for m in self.motors]
         for t in range(self.npos):
             A = self.stepsize * np.sqrt(t/np.pi)
             B = np.sqrt(4*np.pi*t)
-            yield {self.motors[0].name: self.old_pos[0] + A * np.cos(B),
-                   self.motors[1].name: self.old_pos[1] + A * np.sin(B)}
-
-    def run(self):
-        """
-        Override this just to get back to the starting positions afterwards.
-        """
-        self.old_pos = [m.position() for m in self.motors]
-        super(SpiralScan, self).run()
-        # wait for motors then move them back
-        while True in [m.busy() for m in self.motors]:
-            time.sleep(.01)
-        print('Returning motors to their starting positions...')
-        for m, pos in zip(self.motors, self.old_pos):
-            m.move(pos)
-        while True in [m.busy() for m in self.motors]:
-            time.sleep(.01)
-        print('...done')
+            yield {self.motors[0].name: starting[0] + A * np.cos(B),
+                   self.motors[1].name: starting[1] + A * np.sin(B)}
