40dc3f39cd93bbd05112febf4ccadf41dbb44b45
nanomax user <nanomax-user@b-nanomax-controlroom-cc-2.maxiv.lu.se>
2019-09-27 12:37:26 +0200

    Added a software hack to prevent the npoint instability from breaking everything

--- contrast/detectors/LC400Buffer.py
+++ contrast/detectors/LC400Buffer.py
@@ -27,11 +27,50 @@ class LC400Buffer(Detector):
     def busy(self):
         return not (self.proxy.State() in (PyTango.DevState.STANDBY, PyTango.DevState.ON))
 
+    def __emergency_backup(self):
+        # grab these values in case we have to restart and reset
+        grab_keys = ("FlyScanMotorStartPosition", "FlyScanMotorEndPosition", "NumberOfIntervals", 
+                     "GateWidth", "GateLatency", "FlyScanMotorAxis")
+        self.sc_params = {k:self.proxy.read_attribute(k).value for k in grab_keys}
+
+    def __emergency_recover(self):
+        ec0 = PyTango.DeviceProxy('tango/admin/b-v-nanomax-ec-0')
+        ioc = PyTango.DeviceProxy('tango/admin/b303a-a100380-cab03-ctl-ioc-01a')
+        ec0.HardKillServer('LC400ScanControl/B303A')
+        ioc.HardKillServer('NpointLC400/B303A')
+        print('Killing the npoint devices and waiting...')
+        for i in range(10):
+            print('*')
+            time.sleep(1)
+        ioc.DevStart('NpointLC400/B303A')
+        print('Starting the npoint motor device and waiting...')
+        for i in range(10):
+            print('*')
+            time.sleep(1)
+        ec0.DevStart('LC400ScanControl/B303A')
+        print('Starting the npoint scancontrol device and waiting...')
+        for i in range(10):
+            print('*')
+            time.sleep(1)
+        self.initialize()
+        for k, v in self.sc_params.items():
+            self.proxy.write_attribute(k, v)
+        self.proxy.ConfigureLC400Motion()
+        self.proxy.ConfigureLC400Recorder()
+        self.proxy.ConfigureStanford()
+
     def read(self):
-        self.proxy.ReadLC400Buffer()
-        data = {1: self.proxy.Axis1Positions,
-                2: self.proxy.Axis2Positions,
-                3: self.proxy.Axis3Positions,}
+        self.__emergency_backup()
+        try:
+            self.proxy.ReadLC400Buffer()
+            data = {1: self.proxy.Axis1Positions,
+                    2: self.proxy.Axis2Positions,
+                    3: self.proxy.Axis3Positions,}
+            self.length = len(data[1])
+        except PyTango.DevFailed:
+            self.__emergency_recover()
+            fake = np.ones(self.length, dtype=np.float) * -1
+            data = {i: fake for i in (1,2,3)}
         return {'x': data[self.xaxis], 'y': data[self.yaxis], 'z': data[self.zaxis]}
 
     def start(self):
