07466cf8c24009c0f54a22ed47b3d2a615e3b13a
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-10-01 21:35:30 +0200

    Recorders are killed on exit to avoid orphaned subprocesses

--- contrast/recorders/__init__.py
+++ contrast/recorders/__init__.py
@@ -9,3 +9,11 @@ try:
 except ImportError:
     print("Note: Hdf5Recorder not available, probably because h5py is missing.")
 
+import os, signal
+def kill_all_recorders():
+    for r in Recorder.getinstances():
+        print("Killing %s" % r.name)
+        os.kill(r.pid, signal.SIGTERM)
+
+import atexit
+atexit.register(kill_all_recorders)
