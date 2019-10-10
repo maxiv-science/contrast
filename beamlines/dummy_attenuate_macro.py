ea3fbe80ce368364d6cba33462f918cbe50606f2
maikah <maik.kahnt@maxiv.lu.se>
2019-10-10 14:18:24 +0200

    made absorber settings class attributes

--- beamlines/dummy_attenuate_macro.py
+++ beamlines/dummy_attenuate_macro.py
@@ -1,7 +1,29 @@
+"""
+
+:platform: Unix, Windows
+:synopsis: macro to automatically absorb X percent of the bea using the 
+           absorbers at the NanoMAX beamline
+:last changed: 2019-10-10
+:author: Maik Kahnt <maik.kahnt@maxiv.lu.se>
+
+imports:
+--------
+
+* os
+* numpy
+* contrast
+
+"""
 
 import os
 import numpy as np
 from contrast.environment import macro, register_shortcut, runCommand
+
+#   ToDo
+#       - read actual photon energy
+#       - activate that motor move commands are actually run
+#       - check loading of non-code files
+#       - avoid elements with absorption edges close to the current energy
 
 @macro
 class attenuate(object):
@@ -10,44 +32,46 @@ class attenuate(object):
     on the current photon beam enegery.
     """
 
+    ############################################################################
+    #   absorber settings at the NanoMAX beamline - status 2019-10-06 
+    ############################################################################
+
+    elements  = ['Al', 'Ti', 'Si', 'Cu', None, 'Fe', 'Mo', 'Ta', 'Ag']
+    position  = [-40000, -29000, -18000, -9000, 0, 11000,  21000,  33000, 41000]
+    carriers  = ['attenuator1_x', 'attenuator2_x', 
+                 'attenuator3_x', 'attenuator4_x'] 
+    thickness = [[  25,   50,  100,  100],   # in um
+                 [  20,   40,   80,  160],
+                 [ 250,  500, 1000, 1000],
+                 [  20,   40,   80,  160],
+                 [   0,    0,    0,    0],
+                 [  50,  100,  200,  400],
+                 [  15,   30,   60,  120],
+                 [  20,   40,   80,  160],
+                 [  25,   50,  100,  200]]
+    thickness = np.array(thickness)
+
+    ############################################################################
+    #   loading offline data between 5 and 25 keV 
+    #   taken from http://henke.lbl.gov/optical_constants/filter2.html
+    ############################################################################
+
+    absorption_data = {}
+    base  = os.path.dirname(os.path.realpath(__file__))                     #fixme: might change
+    base += '/attenuation/attenuation_1um_'
+    for element in [x for x in elements if not(x==None)]:
+        fpath = base + element + '.txt'
+        data  = np.loadtxt(fpath, skiprows=2)
+        absorption_data[element] = data
+
+    ############################################################################
+    #   methods
+    ############################################################################
+
     def __init__(self, attenuate_to=None, how='safe', verbosity=3):
         self.attenuate_to = attenuate_to
         self.how          = how
         self.verbosity    = verbosity
-
-        self.load_settings()
-        self.load_absorption_data()
-
-    def load_settings(self):
-        # status 2019-10-06
-        self.elements  = ['Al', 'Ti', 'Si', 'Cu', None, 'Fe', 'Mo', 'Ta', 'Ag']
-        self.position  = [-40000, -29000, -18000, -9000, 0, 
-                           11000,  21000,  33000, 41000]
-        self.carriers  = ['att_1', 'att_2', 'att_3', 'att_4'] 
-        self.thickness = [[  25,   50,  100,  100],   # in um
-                          [  20,   40,   80,  160],
-                          [ 250,  500, 1000, 1000],
-                          [  20,   40,   80,  160],
-                          [   0,    0,    0,    0],
-                          [  50,  100,  200,  400],
-                          [  15,   30,   60,  120],
-                          [  20,   40,   80,  160],
-                          [  25,   50,  100,  200]]
-        self.thickness = np.array(self.thickness)
-
-    def load_absorption_file(self, fpath):
-        return np.loadtxt(fpath, skiprows=2)
-
-    def load_absorption_data(self):
-        # loading offline data between 5 and 25 keV 
-        # taken from http://henke.lbl.gov/optical_constants/filter2.html
-        self.absorption_data = {}
-        base  = os.path.dirname(os.path.realpath(__file__))                     #fixme: might change
-        base += '/attenuation/attenuation_1um_'
-        for element in [x for x in self.elements if not(x==None)]:
-            fpath = base + element + '.txt'
-            data  = self.load_absorption_file(fpath)
-            self.absorption_data[element] = data
 
     def get_current_energy(self):
         self.photon_energy = 12000                                              #fixme: get energy information from the beamline
@@ -81,6 +105,7 @@ class attenuate(object):
 
     def run_command(self, command):
         print('currently only printing:', command)                              #fixme: actually run the commands
+        #runCommand(command)
 
     def run(self):
         self.get_current_energy()
