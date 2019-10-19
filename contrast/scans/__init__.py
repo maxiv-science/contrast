4642bf6ef2930aead980860a122f6802d0887553
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2019-10-20 00:00:01 +0200

    bug: left an import statement after moving a module

--- contrast/scans/__init__.py
+++ contrast/scans/__init__.py
@@ -2,5 +2,4 @@ from .Scan import SoftwareScan, Ct, LoopScan
 from .Mesh import Mesh, DMesh
 from .AScan import AScan, DScan
 from .Tweak import Tweak
-from .NpointFlyscan import NpointFlyscan
 from .Spiral import SpiralScan
