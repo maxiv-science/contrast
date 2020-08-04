b3c86d2c7dda6f71c88531b6267bf20014550793
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2020-08-04 18:06:25 +0200

    PandaBox detector works nicely now

--- contrast/detectors/PandaBox.py
+++ contrast/detectors/PandaBox.py
@@ -18,6 +18,7 @@ import time
 import numpy as np
 import socket
 from threading import Thread
+import select
 
 SOCK_RECV = 4096
 
@@ -35,6 +36,7 @@ class PandaBox(Detector, TriggeredDetector, BurstDetector):
         Detector.__init__(self, name=name)
         TriggeredDetector.__init__(self)
         BurstDetector.__init__(self)
+        self.burst_latency = .003 # placeholder to avoid 0s
 
     def initialize(self):
         self.ctrl_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,20 +45,22 @@ class PandaBox(Detector, TriggeredDetector, BurstDetector):
         self.ctrl_sock.connect((self.host, self.ctrl_port))
 
     def query(self, cmd):
-        self.sock.sendall(bytes(cmd + '\n', 'ascii'))
-        return self.sock.recv(SOCK_RECV).decode()
+        self.ctrl_sock.sendall(bytes(cmd + '\n', 'ascii'))
+        return self.ctrl_sock.recv(SOCK_RECV).decode()
 
     def busy(self):
-        # TODO: possibly disarm here
         if self.acqthread and self.acqthread.is_alive():
             return True
+        else:
+            return False
 
     def prepare(self, acqtime, dataid, n_starts):
         """
         Run before acquisition, once per scan.
         """
-        # TODO: set the number of images and the timing
-        pass
+        self.query('PULSE1.PULSES=%d' % self.burst_n)
+        self.query('PULSE1.WIDTH=%f' % acqtime)
+        self.query('PULSE1.STEP=%f' % (self.burst_latency+acqtime))
 
     def arm(self):
         """
@@ -64,35 +68,70 @@ class PandaBox(Detector, TriggeredDetector, BurstDetector):
         """
         self.acqthread = Thread(target=self._acquire)
         self.acqthread.start()
-        # TODO: arm the PCAP block here
-        pass
+        self.query('*PCAP.ARM=')
+        done = False
+        while not done:
+            ret = self.query('*PCAP.COMPLETION?')
+            if 'Busy' in ret:
+                done = True
+            time.sleep(.005)
+        time.sleep(.05) # necessary hack
 
-    def _acquire(self, N):
+    def _acquire(self):
         """
         Receive and parse one set of measurements.
         """
-        # TODO: connect and get the header, collect the channels
-        data = {ch:[] for ch in channels}
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.data_port))
+        s.send(b'\n\n')
+
+        # First wait for the header to be complete, and parse it.
+        done = False
+        buff = b''
+        while not done:
+            buff += s.recv(SOCK_RECV)
+            if b'\n\n' in buff:
+                done = True
+        header, buff = buff.split(b'\n\n')
+        channels = []
+        for line in header.split(b'fields:\n')[-1].split(b'\n'):
+            ch = line.strip().split()[0].decode()
+            op = line.strip().split()[2].decode()
+            channels.append(ch + '_' + op)
+
+        # Then put the rest of the data into the same buffer and continue
         n = 0
+        data = {ch:[] for ch in channels}
         while n < self.burst_n:
-            # TODO: receive everything and append to the lists in data
-            pass
+            # anything more to read?
+            ready = select.select([s], [], [], .1)[0]
+            if ready:
+                buff += s.recv(SOCK_RECV)
+
+            #anything more to parse?
+            if b'\n' in buff:
+                line, buff = buff.split(b'\n', 1)
+                vals = line.strip().split()
+                for k, v in zip(channels, vals):
+                    if b'END' in v:
+                        data[k].append(None)
+                    data[k].append(float(v))
+                n += 1
+
         self.data = data
+        self.query('*PCAP.DISARM=')
 
     def start(self):
         """
         Start acquisition when software triggered.
         """
         if not self.hw_trig:
-            pass
-            # TODO: here we should flicker the magic bit
-        else:
-            pass
-            # do nothing, we'll get our trigger.
+            self.query('BITS.A=1')
+            time.sleep(.001)
+            self.query('BITS.A=0')
 
     def stop(self):
-        # TODO: disarm
-        pass
+        self.query('*PCAP.DISARM=')
 
     def read(self):
-        # TODO: return the dict parsed from the streamer
+        return self.data
