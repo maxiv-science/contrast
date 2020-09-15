0b5997fcdcc00ad2f8ea4a2f7325a1917409fa08
nanomax user <nanomax-user@b-nanomax-controlroom-cc-2.maxiv.lu.se>
2020-09-15 21:58:20 +0200

    removed remaining merge conflict

--- beamlines/nanomax/NpointFlyscan.py
+++ beamlines/nanomax/NpointFlyscan.py
@@ -94,14 +94,9 @@ class NpointFlyscan(Mesh):
                 self.fastmotor.proxy.start_waveform()
                 ok = True
             except:
-<<<<<<< HEAD
                 n += 1
                 if n >= 10:
                     print('***** start_waveform() failed %u times, is the piexo having trouble settling? trying again...'%n)
-=======
-                print('***** scancontrol Go() failed, is the piezo having trouble settling? trying again...')
-                import time
->>>>>>> master
                 time.sleep(.1)
 
     def _while_acquiring(self):
