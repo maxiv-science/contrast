64cf7f93e77dc6ddf56c3600af7e288f0623f576
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2020-10-19 17:45:16 +0200

    first version of the Andor3 streamed detector

---
+++ contrast/detectors/Andor3.py
@@ -0,0 +1,99 @@
+if __name__ == '__main__':
+    from contrast.detectors.Detector import Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector
+    from contrast.environment import env
+    from contrast.recorders.Hdf5Recorder import Link
+else:
+    from .Detector import Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector
+    from ..environment import env
+    from ..recorders.Hdf5Recorder import Link
+import os
+import PyTango
+
+class Andor3(Detector, SoftwareLiveDetector, BurstDetector):
+    """
+    Detector class interfacing directly with the andor-streamer:
+
+    https://gitlab.maxiv.lu.se/nanomax-beamline/andor-streamer
+
+    Note: hw trigger not yet implemented.
+    """
+
+    def __init__(self, device='zyla/test/1', name=None):
+        SoftwareLiveDetector.__init__(self)
+        BurstDetector.__init__(self)
+        Detector.__init__(self, name=name) # last so that initialize() can overwrite parent defaults
+        self.proxy = PyTango.DeviceProxy(device)
+
+    def initialize(self):
+        pass
+
+    def prepare(self, acqtime, dataid, n_starts):
+        """
+        Run before acquisition, once per scan. Set up triggering,
+        number of images etc.
+        """
+
+        if self.busy():
+            raise Exception('%s is busy!' % self.name)
+
+        # saving and paths
+        if dataid is None:
+            # no saving
+            self.saving_file = ''
+            self.proxy.filename = ''
+        else:
+            # saving
+            path = env.paths.directory
+            fn = 'scan_%06d_%s.hdf5' % (dataid, self.name)
+            self.saving_file = os.path.join(path, fn)
+            if os.path.exists(self.saving_file):
+                print('%s: this hdf5 file exists, I am raising an error now'%self.name)
+                raise Exception('%s hdf5 file already exists' % self.name)
+            self.proxy.filename = self.saving_file
+
+        # arming and numbers of frames
+        self.proxy.exposure_time = acqtime
+        if self.burst_n == 1:
+            self.proxy.start()
+            self.proxy.trigger_mode = 'Software'
+            self.proxy.frame_count = n_starts
+        else:
+            self.proxy.trigger_mode = 'Internal'
+            self.proxy.frame_count = self.burst_n
+        self.frames_expected = 0
+
+    def arm(self):
+        """
+        Called on every software step - no action for sw timing
+        """
+        pass
+
+    def start(self):
+        """
+        Start acquisition when software triggered.
+        """
+        if self.burst_n == 1:
+            self.proxy.software_trigger()
+            self.frames_expected += 1
+        else:
+            self.proxy.start()
+            self.frames_expected = self.burst_n
+
+    def stop(self):
+        self.proxy.stop()
+
+    def busy(self):
+        st = self.proxy.State()
+        if st in (PyTango.DevState.STANDBY, PyTango.DevState.ON):
+            return False
+        elif st == PyTango.DevState.RUNNING:
+            if self.proxy.acquired_frames == self.frames_expected:
+                return False
+        return True
+
+    def read(self):
+        if self.saving_file == '':
+            return None
+        else:
+            return Link(self.saving_file , 'entry/instrument/andor', universal=True)
+
