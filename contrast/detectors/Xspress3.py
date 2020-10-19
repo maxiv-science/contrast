c05252b82e192b75548cc0a487346e952203aa77
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2020-10-19 15:36:49 +0200

    First version of the Xspress3 streamer detector

---
+++ contrast/detectors/Xspress3.py
@@ -0,0 +1,114 @@
+from .Detector import Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector
+from ..environment import env
+from ..recorders.Hdf5Recorder import Link
+import os
+import PyTango
+
+class Xspress3(Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector):
+    """
+    Detector class interfacing directly with the xspress3-streamer:
+
+    https://gitlab.maxiv.lu.se/nanomax-beamline/xspress3-streamer
+    """
+
+    def __init__(self, device='staff/alebjo/xspress3', name=None):
+        SoftwareLiveDetector.__init__(self)
+        TriggeredDetector.__init__(self)
+        BurstDetector.__init__(self)
+        Detector.__init__(self, name=name) # last so that initialize() can overwrite parent defaults
+        self.proxy = PyTango.DeviceProxy(device)
+
+    def initialize(self):
+        self.burst_latency = 100e-9
+
+    def prepare(self, acqtime, dataid, n_starts):
+        """
+        Run before acquisition, once per scan. Set up triggering,
+        number of images etc.
+        """
+
+        if self.busy():
+            raise Exception('%s is busy!' % self.name)
+
+        # saving and paths
+        if dataid is None:
+            # no saving
+            self.saving_file = ''
+            self.proxy.WriteHdf5 = False
+        else:
+            # saving
+            path = env.paths.directory
+            fn = 'scan_%06d_%s.hdf5' % (dataid, self.name)
+            self.saving_file = os.path.join(path, fn)
+            if os.path.exists(self.saving_file):
+                print('%s: this hdf5 file exists, I am raising an error now'%self.name)
+                raise Exception('%s hdf5 file already exists' % self.name)
+            self.proxy.DestinationFileName = self.saving_file
+            self.proxy.WriteHdf5 = True
+
+        # arming and numbers of frames
+        if self.burst_n > 1:
+            acqtime -= self.burst_latency
+        self.proxy.ExposureTime = acqtime
+        self.proxy.nFramesPerTrigger = self.burst_n
+        self.proxy.LatencyTime = self.burst_latency
+        if self.hw_trig:
+            self.proxy.TriggerMode = 'EXTERNAL'
+        else:
+            self.proxy.TriggerMode = 'SOFTWARE'
+
+        # if not in burst mode, we can arm here and then just soft trigger
+        ntrig = 1
+        self.arm_calls = 0
+        if self.burst_n == 1:            
+            ntrig *= n_starts
+        if self.hw_trig:
+            ntrig *= self.hw_trig_n
+        self.proxy.nTriggers = ntrig
+        if self.burst_n == 1:
+            self.proxy.Arm()
+
+        # so how many frames do we expect to see before the detector is done with a specific start()?
+        exp = self.burst_n
+        if self.hw_trig:
+            exp *= self.hw_trig_n
+        self.expected_per_arm = exp
+        self.expected_total = 0
+
+    def arm(self):
+        """
+        Called on every software step
+        """
+        # in burst mode, we have to arm here, otherwise it's already done
+        if self.burst_n > 1:
+            self.proxy.Arm()
+            self.expected_total = self.expected_per_arm
+        else:
+            self.expected_total += self.expected_per_arm
+
+    def start(self):
+        """
+        Start acquisition when software triggered.
+        """
+        if self.hw_trig:
+            return
+        self.proxy.SoftwareTrigger()
+
+    def stop(self):
+        self.proxy.Stop()
+
+    def busy(self):
+        st = self.proxy.State()
+        if st == PyTango.DevState.STANDBY:
+            return False
+        elif st == PyTango.DevState.RUNNING:
+            if self.proxy.nFramesAcquired == self.expected_total:
+                return False
+        return True
+
+    def read(self):
+        if self.saving_file == '':
+            return None
+        else:
+            return Link(self.saving_file , '/', universal=True)
+
