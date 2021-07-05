corrected the current calculation formula

contrast/detectors/AlbaEM.py
@@ -272,7 +272,7 @@ class AlbaEM(Detector, LiveDetector, TriggeredDetector, BurstDetector):
         if self.global_arm:
             # case 1 or 4, which means read a single point each time
             data = np.array(self.stream.data[-1])
-            data[1:] = data[1:] / ti_gains / v_gains / (self.acqtime / 40e-9) * 1e-6 # A
+            data[1:] = data[1:] / ti_gains / v_gains / (self.acqtime / 40e-9) * 1e-3 # A
             return {k:v for k,v in zip(keys, data)}
         else:
             # case 2 or 3, return the last burst_n or hw_trig_n points
@@ -280,7 +280,7 @@ class AlbaEM(Detector, LiveDetector, TriggeredDetector, BurstDetector):
             assert (len(self.stream.data) == chunk * self.n_started), 'wrong number of points received'
             ret = {}
             data = np.array(self.stream.data[-chunk:])
-            data[:, 1:] = data[:, 1:] / ti_gains / v_gains / (self.acqtime / 40e-9) * 1e-6 # A
+            data[:, 1:] = data[:, 1:] / ti_gains / v_gains / (self.acqtime / 40e-9) * 1e-3 # A
             for i in range(len(keys)):
                 ret[keys[i]] = data[:, i].reshape((1,-1))
             return ret