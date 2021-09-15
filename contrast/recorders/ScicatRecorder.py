d2ad11bc4cf0cd90392a5c797677fb7873e5580d
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2021-09-15 15:06:12 +0200

    Improvements on the ScicatRecorder

--- contrast/recorders/ScicatRecorder.py
+++ contrast/recorders/ScicatRecorder.py
@@ -6,35 +6,45 @@ class ScicatRecorder(Recorder):
     """
     Recorder which submits acquisition info to the SciCat database via a helper library.
     """
-    def __init__(self, name=None):
+    def __init__(self, name=None, verbose=False):
         Recorder.__init__(self, name=name)
+        self.verbose = verbose
 
     def act_on_header(self, dct):
         """
-        Give SciCat all the info for this scan.
+        Give SciCat all the info for this scan. Enters the whole snapshot.
         """
         self.entry = SciFish()
         self.entry.start_scan()
         self.entry.scicat_data.datasetName = dct['scannr']
-        #self.entry.scicat_data.sampleId = ""
+        self.entry.scicat_data.sampleId = dct['scannr']
         #self.entry.scicat_data.dataFormat = ""
         #self.entry.scicat_data.files = []
         #self.entry.scicat_data.sourceFolder = "" # default from sdm
         #self.entry.scicat_data.description = ""
         self.entry.environment_data.title = dct['description']
         self.entry.environment_data.scanID = dct['scannr']
-        det_group = Detector.get_active()
-        self.entry.environment_data.detectors = sorted([d.name for d in det_group])
+
+        self.entry.scicat_data.scientificMetadata.update(dct['snapshot'])
+
+        self.posted_detectors = False
 
     def act_on_data(self, dct):
         """
         Only scan-level information is sent.
         """
-        pass
+        if not self.posted_detectors:
+            dets = list(dct.keys())
+            # should really remove non-detector things here
+            # (search the description? look at the forked Gadget tree?)
+            self.entry.environment_data.detectors = sorted(dets)
+            self.posted_detectors = True
 
     def act_on_footer(self, dct):
         """
         Tell SciCat that the scan is done
         """
+        if self.verbose:
+            print('ScicatRecorder posted this to the database:')
+            print(self.entry.show())
         self.entry.end_scan()
-
