0324edeef507b1489e3c67fc87acf6c93c857a9a
nanomax user <nanomax-user@b-nanomax-controlroom-cc-4.maxiv.lu.se>
2022-03-09 11:46:44 +0100

    added working Fermat Scan

--- contrast/scans/__init__.py
+++ contrast/scans/__init__.py
@@ -6,4 +6,4 @@ from .Scan import SoftwareScan, Ct, LoopScan
 from .Mesh import Mesh, DMesh
 from .AScan import AScan, DScan
 from .Tweak import Tweak
-from .Spiral import SpiralScan
+from .Spiral import SpiralScan, FermatScan
