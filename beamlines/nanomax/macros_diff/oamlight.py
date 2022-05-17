ec539c1202c086cdb2eae928a555c9299d9e0522
nanomax user <nanomax-user@b-nanomax-controlroom-cc-3.maxiv.lu.se>
2022-05-17 10:24:44 +0200

    fixed imports, added important oamlight functionality

--- beamlines/nanomax/macros_diff/oamlight.py
+++ beamlines/nanomax/macros_diff/oamlight.py
@@ -1,12 +1,15 @@
 from contrast.environment import MacroSyntaxError, env, macro, register_shortcut, runCommand
 import requests
+import time
+import random
+
 
 @macro
 class Oamlight(object):
     """
     Macro for the control of the LED ring on the On-axis microscope.
 
-    oamlight <on|off>   Set all LEDs of the ring on or off
+    oamlight <on|off|disco>   Set all LEDs of the ring on, off, or to party mode
     oamlight <value>    Set the brightness of all LEDs, <value> is in percent [0-100]
     oamlight <left|top|right> <on|off>  Set the predefined LED group on or off
     oamlight <left|top|right> <value>   Set the brighntess of the predefined LED group to <value> (in percent [0-100])
@@ -30,20 +33,24 @@ class Oamlight(object):
                   'top': [4,5,6],
                   'left': [7,8,9]}
     MAXBRIGHTNESS = 3800 # can be upto 4095, but this may burn out the LEDs too fast
-    
+
     def __init__(self, *args):
         self.args = args
         self.session = requests.Session()
         self.session.trust_env = False
-    
+
     def run(self):
         if len(self.args) == 0:
             self.show_led_status()
         # if only one argument is given, set the brighness of the leds in the 'all' led group
         elif len(self.args) == 1:
-            group = 'all'
-            brightness = self.to_raw(self.args[0])
-            self.set_group(self.led_groups[group], brightness)
+            if self.args[0] == 'disco':
+                self.disco()
+            else:
+                group = 'all'
+                brightness = self.to_raw(self.args[0])
+                print(brightness, type(brightness))
+                self.set_group(self.led_groups[group], brightness)
         # if an even number of arguments is given, it must be a pair of a LED group and brightness or a list of LEDs and brightness
         elif len(self.args)%2 == 0:
             for i in range(0, len(self.args), 2):
@@ -60,8 +67,7 @@ class Oamlight(object):
                     print(f"{self.args[i]} is not a valid LED identifier.")
         else:
             raise MacroSyntaxError("Wrong number of arguments")
-         
-         
+
     def show_led_status(self):
         # get raw data from the led controller
         led_status = self.get_leds()
@@ -72,7 +78,7 @@ class Oamlight(object):
             i=i+1
             if i >= len(self.led_groups['all']):
                 break           
-                
+
     def to_raw(self, value):
         if value == 'on':
             brightness = self.MAXBRIGHTNESS
@@ -98,14 +104,29 @@ class Oamlight(object):
             payload[led_chan-1] = brightness
             
         self.set_leds(payload)
-        
+
     def set_leds(self, payload:dict):
         key = "led"
         url = f'http://{self.host}:{self.port}/{key}'
         response = self.session.put(url,json=payload)
-        
+
     def get_leds(self):
         key = "led"
         url = f'http://{self.host}:{self.port}/{key}'
         response = self.session.get(url)
         return response.json()
+
+    def disco(self):
+        try:
+            while True:
+                i0, i1 = random.sample(self.led_groups['all'], 2)
+                self.set_group([i0], self.MAXBRIGHTNESS)
+                self.set_group([i1], self.MAXBRIGHTNESS)
+                time.sleep(.1)
+                self.set_group([i0], 0)
+                self.set_group([i1], 0)
+        except KeyboardInterrupt:
+            b = self.to_raw(30)
+            self.set_group(self.led_groups['all'], b)
+            print('party over!')
+
