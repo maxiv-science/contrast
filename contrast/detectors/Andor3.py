7da24f646d64a6720c2158469ce86d5895411eb1
Igor Beinik <igor.beinik@maxiv.lu.se>
2022-11-16 12:51:45 +0100

    Merging: reset unwarranted changes to Andor3

--- contrast/detectors/Andor3.py
+++ contrast/detectors/Andor3.py
@@ -1,23 +1,15 @@
-import os
-import PyTango
-import datetime
-import time
-import logging as log
-import signal
-log.basicConfig(filename='andor_log.txt',
-                filemode='w+',
-                format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                datefmt='%H:%M:%S',
-                level=log.DEBUG)
-
 if __name__ == '__main__':
-    from contrast.detectors.Detector import Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector
+    from contrast.detectors.Detector import (
+        Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector)
     from contrast.environment import env
     from contrast.recorders.Hdf5Recorder import Link
 else:
-    from .Detector import Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector
+    from .Detector import (
+        Detector, SoftwareLiveDetector, TriggeredDetector, BurstDetector)
     from ..environment import env
     from ..recorders.Hdf5Recorder import Link
+import os
+import PyTango
 
 
 class Andor3(Detector, SoftwareLiveDetector, BurstDetector):
@@ -32,7 +24,8 @@ class Andor3(Detector, SoftwareLiveDetector, BurstDetector):
     def __init__(self, device='zyla/test/1', name=None):
         SoftwareLiveDetector.__init__(self)
         BurstDetector.__init__(self)
-        Detector.__init__(self, name=name) # last so that initialize() can overwrite parent defaults
+        # last so that initialize() can overwrite parent defaults:
+        Detector.__init__(self, name=name)
         self.proxy = PyTango.DeviceProxy(device)
 
     def initialize(self):
@@ -46,26 +39,27 @@ class Andor3(Detector, SoftwareLiveDetector, BurstDetector):
         if dataid is None:
             # no saving
             self.saving_file = ''
-            self.proxy.filename = ''
+            self.proxy.Filename = ''
         else:
             # saving
             path = env.paths.directory
             fn = 'scan_%06d_%s.hdf5' % (dataid, self.name)
             self.saving_file = os.path.join(path, fn)
             if os.path.exists(self.saving_file):
-                print('%s: this hdf5 file exists, I am raising an error now'%self.name)
+                print('%s: this hdf5 file exists, I am raising an error now'
+                      % self.name)
                 raise Exception('%s hdf5 file already exists' % self.name)
-            self.proxy.filename = self.saving_file
+            self.proxy.Filename = self.saving_file
 
         # arming and numbers of frames
-        self.proxy.exposure_time = acqtime
+        self.proxy.ExposureTime = acqtime
         if self.burst_n == 1:
-            self.proxy.trigger_mode = 'SOFTWARE'
-            self.proxy.frame_count = n_starts
-            self.proxy.start()
+            self.proxy.TriggerMode = 'Software'
+            self.proxy.nTriggers = n_starts
+            self.proxy.Arm()
         else:
-            self.proxy.trigger_mode = 'INTERNAL'
-            self.proxy.frame_count = self.burst_n
+            self.proxy.TriggerMode = 'Internal'
+            self.proxy.nTriggers = self.burst_n
         self.frames_expected = 0
 
     def arm(self):
@@ -73,21 +67,21 @@ class Andor3(Detector, SoftwareLiveDetector, BurstDetector):
 
     def start(self):
         if self.burst_n == 1:
-            self.proxy.software_trigger()
+            self.proxy.SoftwareTrigger()
             self.frames_expected += 1
         else:
-            self.proxy.start()
+            self.proxy.Arm()
             self.frames_expected = self.burst_n
 
     def stop(self):
-        self.proxy.stop()
+        self.proxy.Stop()
 
     def busy(self):
         st = self.proxy.State()
         if st in (PyTango.DevState.STANDBY, PyTango.DevState.ON):
             return False
         elif st == PyTango.DevState.RUNNING:
-            if self.proxy.acquired_frames == self.frames_expected:
+            if self.proxy.nFramesAcquired == self.frames_expected:
                 return False
         return True
 
@@ -95,5 +89,5 @@ class Andor3(Detector, SoftwareLiveDetector, BurstDetector):
         if self.saving_file == '':
             return None
         else:
-            return Link(self.saving_file , 'entry/instrument/andor', universal=True)
-
+            return Link(
+                self.saving_file, 'entry/instrument/andor', universal=True)
