a444d1d4511a8e19677a08bd1a04820116bd6199
Maik Kahnt <maik.kahnt@maxiv.lu.se>
2022-12-12 15:31:44 +0100

    unified the cosaxs and nanoamx eiger solutions

--- beamlines/cosaxs/cosaxs.py
+++ beamlines/cosaxs/cosaxs.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
     # detectors
     ########################################
 
-    eiger4m = Eiger(name='eiger4m', host='b-cosaxs-eiger-dc-0',  use_image_appendix=True) # 172.16.197.26
+    eiger4m = Eiger(name='eiger4m', host='b-cosaxs-eiger-dc-0',  use_image_appendix=True, hdf_path='entry/instrument/eiger/data') # 172.16.197.26
     panda0 = PandaBox(name='panda0', host='b-cosaxs-pandabox-0') # 172.16.198.70
     alba0 = AlbaEM(name='alba0', host='172.16.198.48') #172.16.198.48 # maybe channel 2
     pseudo = PseudoDetector(name='pseudo',
