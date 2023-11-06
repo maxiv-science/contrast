a5f6d2fee90c432a3b2ef4cdf756066f03bfc9f3
Maik Kahnt <maik.kahnt@maxiv.lu.se>
2023-11-06 12:47:50 +0100

    added an absolute spiral scan as for user wish

--- contrast/scans/Spiral.py
+++ contrast/scans/Spiral.py
@@ -44,6 +44,36 @@ class SpiralScan(DScan):
                    self.motors[1].name: starting[1] + A * np.sin(B)}
 
 @macro
+class SpiralAScan(DScan):
+    """
+    Software scan across a 2D Archimedes spiral centered on a 
+    given position. :
+        
+        spiralscan <motor1> <pos1> <motor2> <pos2> <stepsize> <positions> <exp_time>
+    """
+
+    def __init__(self, m1, pos1,  m2, pos2, stepsize, npos, exptime, **kwargs):
+        # Parse arguments. We're inheriting DScan to get its nice run()
+        # method, but we'll call the SoftwareScan constructor anyway bacause
+        # we're not interested in DScan's way of parsing arguments.
+        try:
+            SoftwareScan.__init__(self, float(exptime))
+            self.motors = [m1, m2]
+            self.starting = [pos1, pos2]
+            self.stepsize = float(stepsize)
+            self.n_positions = int(npos)
+            assert all_are_motors(self.motors)
+        except:
+            raise MacroSyntaxError
+
+    def _generate_positions(self):
+        for t in range(self.n_positions):
+            A = self.stepsize * np.sqrt(t/np.pi)
+            B = np.sqrt(4*np.pi*t)
+            yield {self.motors[0].name: self.starting[0] + A * np.cos(B),
+                   self.motors[1].name: self.starting[1] + A * np.sin(B)}
+
+@macro
 class FermatScan(AScan):
     """
     Software scan in the shape of a fermat spiral (good for ptychography scan).
