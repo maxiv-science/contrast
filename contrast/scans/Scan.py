3c53a3f6d992c688303312e4766350a683dd0f8a
Maik Kahnt <maik.kahnt@maxiv.lu.se>
2024-11-25 16:19:00 +0100

    cleanup diff

--- contrast/scans/Scan.py
+++ contrast/scans/Scan.py
@@ -145,6 +145,8 @@ class SoftwareScan(object):
         this sends a message prior to the scan with how many triggers there will be in the scan
         ... this fails in scans with more than 100k triggers 
         """ 
+        det_group = Detector.get_active()
+        
         soft_positions = sum(1 for _ in self._generate_positions())
         fly_positions = 1
         if hasattr(self, 'fastmotorintervals'):
@@ -170,7 +172,7 @@ class SoftwareScan(object):
         # find and prepare the detectors
         det_group = Detector.get_active()
 
-        self.arm_dranspose_pipeline()
+        #self.arm_dranspose_pipeline()
 
         trg_group = TriggerSource.get_active()
         group = det_group + trg_group
