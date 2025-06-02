1a8b14d7e15a6b9007c59f55ef2f33c00cf63064
Igor Beinik <igor.beinik@maxiv.lu.se>
2025-06-02 15:22:44 +0200

    status diff end of 20241230

--- beamlines/nanomax/diffraction.py
+++ beamlines/nanomax/diffraction.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     m2fpitch = E727Motor(device='B303A-EH/CTL/PZCU-01', axis=3, name='m2fpitch', userlevel=2, user_format='%.3f', dial_format='%.3f', dial_limits=(0,30))
 
     # Robot
-    #gamma, delta, radius = KukaRobot('B303-EH2/CTL/DM-02-ROBOT', names=['gamma', 'delta', 'radius'])
+    # gamma, delta, radius = KukaRobot('B303-EH2/CTL/DM-02-ROBOT', names=['gamma', 'delta', 'radius'])
 
     # microscope motors through the Pool
     oam_x = TangoMotor(device='b303a-e02/dia/om-01-x', name='oam_x', userlevel=4, user_format='%.4f', dial_format='%.4f')
@@ -337,12 +337,12 @@ if __name__ == '__main__':
     # macros_common.WFtrigscan.dac_2 = sz
 
     # setup of continous energy scanning
-    # panda1 = PandaBoxPCAP("b303a-a100380cab03/dia/panda-01", name='panda1', hdf_path='/entry/instrument/pandabox/data/')
-    # macros_common.EnergyFlyscan.PCAP=panda1
-    # macros_common.EnergyFlyscan.energy_motor=energy
-    # macros_common.EnergyFlyscan.ivu_gap_motor=ivu_gap
-    # macros_common.EnergyFlyscan.energyflyscan_panda = panda1
-    # macros_common.EnergyFlyscan.trigger_distribution_panda = panda0
+    panda1 = PandaBoxPCAP("b303a-a100380cab03/dia/panda-01", name='panda1', hdf_path='/entry/instrument/pandabox/data/')
+    macros_common.EnergyFlyscan.PCAP=panda1
+    macros_common.EnergyFlyscan.energy_motor=energy
+    macros_common.EnergyFlyscan.ivu_gap_motor=ivu_gap
+    macros_common.EnergyFlyscan.energyflyscan_panda = panda1
+    macros_common.EnergyFlyscan.trigger_distribution_panda = panda0
 
     pseudo = PseudoDetector(name='pseudo',
                             variables={'c1': 'panda0/INENC1.VAL_Mean',
@@ -350,15 +350,15 @@ if __name__ == '__main__':
                                        'c3': 'panda0/INENC3.VAL_Mean',
                                        # 'adc2': 'panda0/FMC_IN.VAL2_Mean',
                                        # 'adc3': 'panda0/FMC_IN.VAL3_Mean',
-                                       # 'adc4': 'panda0/FMC_IN.VAL4_Mean',
+                                       'adc4': 'panda0/FMC_IN.VAL4_Mean',
                                        },
                             expression={'x': 'c2', 'y': 'c3', 'z': 'c1',
                                         # 'analog_x': '-adc2*5', 'analog_y': 'adc3*5', 'analog_z': '-adc1*5',
-                                        # 'xbic': 'adc4',
+                                        'xbic': 'adc4',
                                         })
 #                                        'analog_x': '-adc2*5*10/2**31', 'analog_y': 'adc3*5*10/2**31', 'analog_z': '-adc1*5*10/2**31'})
 
-    # The keysight as both a detector (ammeter) and motor (bias voltage)
+    # # The keysight as both a detector (ammeter) and motor (bias voltage)
     # keysight = Keysight2985(name='keysight', device='B303A-EH/CTL/KEYSIGHT-01')
     # keysight_bias = TangoAttributeMotor(name='keysight_bias', device='B303A-EH/CTL/KEYSIGHT-01', attribute='bias_voltage', dial_limits=(-10,10))
     # keysight_range = TangoAttributeMotor(name='keysight_range', device='B303A-EH/CTL/KEYSIGHT-01', attribute='current_range', dial_format='%E', user_format='%E')
@@ -377,7 +377,7 @@ if __name__ == '__main__':
     # default detector selection
     for d in Detector.getinstances():
         d.active = False
-    for d in [panda0, pseudo, alba2]:# :x3mini, eiger1m, ring_current, pilatus]:
+    for d in [panda0, pseudo, alba2]:# :x3mini, keysight, eiger1m, ring_current, pilatus]:
         d.active = True
     #for d in [xspress3, eiger500k, eiger1m, pilatus, alba0, alba1, alba2]: 
     #    d.hw_trig = True
